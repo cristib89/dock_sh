e76a9b60c868202d9a20d24087fb320968767b57
Cristi B <serpisor@localhost.localdomain>
2018-11-17 15:21:04 +0200

    added a status option and rewrote some phrases

--- dock.sh
+++ dock.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
-commands="[start|stop|logs|bye]"
+commands="[start|stop|status|logs|bye]"
 
 echo ">> docker-compose admin script ... "
-echo "<< provide command $commands ..."
+echo ">> supported commands $commands ..."
 while :
 do
 	read INPUT_ARG
 	case $INPUT_ARG in
+		status)
+			echo "<< showing status ..."
+			docker ps
+			echo ">> done ..."
+			;;
     		start)
       			echo "<< starting ..."
       			docker-compose up -d
@@ -23,14 +28,14 @@ do
 		logs)
 			echo "<< showing logs ..."
 			docker-compose logs
-			echo ">> done"
+			echo ">> done ..."
 			;;
 		bye)
 			echo ">> have a nice day ..."
 			break
 			;;
 		*)
-			echo "<< ? , i know how to handle just $commands"
+			echo "<< ? , type in either of these commands: $commands"
 			;;
 	esac
 done
